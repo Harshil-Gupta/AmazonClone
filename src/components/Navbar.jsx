7eec598cb238eab685c2c7cc6c5f04a17aec4ca0
Harshil Gupta <harshilvgupta@gmail.com>
2022-07-12 17:11:24 +0530

    Created Basic Navbar

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import styled from 'styled-components'
+
+const Container = styled.div`
+
+    background-color:black;
+    height:60px;
+
+`
+
+const Navbar = () => {
+  return (
+    <Container/>
+  )
+}
+
+export default Navbar
