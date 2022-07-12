902a1d1a970bdd6c80ff3950b3468d9165efb9f4
Harshil Gupta <harshilvgupta@gmail.com>
2022-07-13 01:52:56 +0530

    .

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import Announcement from '../components/Announcement'
 import Navbar from '../components/Navbar'
+import Sliders from '../components/Sliders'
 
 const Home = () => {
   return (
     <div>
     <Announcement/>
     <Navbar/>
+    <Sliders/>
     </div>
   )
 }
