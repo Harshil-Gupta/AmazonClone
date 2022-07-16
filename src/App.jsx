ec5cc72dffe7d93fb2f35c05822ca21415771957
Harshil Gupta <harshilvgupta@gmail.com>
2022-07-16 18:48:09 +0530

    .

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,12 @@
+import Home from "./pages/Home";
 import Product from "./pages/Product";
-import Home from "./pages/Home"
 import ProductList from "./pages/ProductList";
-import Register from "./pages/Register";
 import Login from "./pages/Login";
+import Register from "./pages/Register";
 import Cart from "./pages/Cart";
 
 const App = () => {
-  return <Cart/>;
+  return <Product/>
 };
 
 export default App;
